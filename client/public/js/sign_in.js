bfbb714c8e07f0d9a6be39b5b53cc1df479812d4
Alexander Alie <alexanderalie@alexanders-mbp.home>
2021-08-03 22:09:39 -0400

    adding more

--- client/public/js/sign_in.js
+++ client/public/js/sign_in.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
 
- $(".submit-bttn").submit(function(){
+ $(".submit-bttn").submit(function(e){
+   e.preventDefault();
    console.log("Hello World");
 
-   
+   let signIn = {
+     username: $('.username-input').val(),
+     password: $('.password-input').val()
+    }
+
+    $.ajax({
+      method:'POST',
+      method:'/api/sign-in',
+      url:'json',
+      data: JSON.strignify(signIn),
+      contentType:'application/json'
+    }).then(function(res){
+      console.log(res);
+    })
  })
 
 });
--- server/controller/routes.js
+++ server/controller/routes.js
@@ -15,6 +15,11 @@ module.exports = (app, passport, db) =>{
     res.sendFile(path.join(__dirname, '../../client/public/html/sign_in.html'));
   })
 
+  app.get('/signed-in', function(req, res){
+    res.sendFile(path.join(__dirname, '../../client/public/signed_in.html'));
+  })
+
+
   app.get('/api/sign-up', function(req, res){
     passport.authenticate('local-signup', function(err, user, info){
       if(err){
@@ -36,24 +41,22 @@ module.exports = (app, passport, db) =>{
   });
 
 
-  app.get('/api/sign-in', function(req, res){
+  app.post('/api/sign-in', function(req, res, next){
     passport.authenticate('local-signin', function(err, user, info){
       if(err){
         return next(err);
-      }else{
-        res.json({user: user, info: info})
       }
-    })(req, res);
+      if(!user){
+        return res.json({success: false, message: 'authentication failed', info: info});
+      }
+      req.login(user, function(err){
+        if(err){
+          return next(err);
+        }
+        return res.status(200).json({success: true, message: 'authentication succeeded', object: user});
+      });
+    })(req, res, next);
   })
-
-
-
-
-
-
-
-
-
 
 
 
